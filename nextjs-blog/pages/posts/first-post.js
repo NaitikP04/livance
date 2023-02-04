9c5b7058643339f44d37537d1f7ca0cf1cc06bcb
Naitik Poddar <30523060+NaitikP04@users.noreply.github.com>
2023-02-03 23:17:50 -0800

    links to each other

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,3 +1,14 @@
+import Link from 'next/link';
+import styles from '../../styles/Home.module.css';
+
 export default function FirstPost() {
-    return <h1>First Post</h1>;
+    return (
+        <div className={styles.container}>
+            <main>
+                <h1 className={styles.title}>
+                Go to <Link href="/">Home Page!</Link>
+                </h1>
+            </main>
+        </div>
+    )
 }
